b99c70a3c7b4ed5a9556b4aa530b9b73e45967bb
Sabrina Andrade <eusabrinaps@icloud.com>
2026-05-31 15:32:53 -0300

    test: findByCpf with non-existing CPF returns empty

--- src/test/java/com/hotel/booking/ifsp/persistence/GuestPersistenceTest.java
+++ src/test/java/com/hotel/booking/ifsp/persistence/GuestPersistenceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Tag("PersistenceTest")
@@ -23,8 +24,17 @@ class GuestPersistenceTest extends PersistenceIntegrationTestBase {
     void FindByCpfWithExistingCpfReturnsGuest() {
         var obtained = guestRepository.findByCpf("123.456.789-09");
 
-        assertTrue(obtained.isPresent());
+        assertThat(obtained).isPresent();
         assertEquals("Carlos Silva", obtained.get().getName());
         assertEquals("123.456.789-09", obtained.get().getCpf());
+    }
+
+    @Test
+    @Tag("PersistenceTest")
+    @Tag("IntegrationTest")
+    @DisplayName("findByCpf with non-existing CPF returns empty")
+    void FindByCpfWithNonExistingCpfReturnsEmpty() {
+        var obtained = guestRepository.findByCpf("529.982.247-25");
+        assertThat(obtained).isEmpty();
     }
 }
