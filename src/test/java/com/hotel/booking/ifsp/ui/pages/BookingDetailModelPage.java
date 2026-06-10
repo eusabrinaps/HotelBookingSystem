216f2e3ce609b38e25892938ba9edada09d30974
Filipe Braga <filipebraga0505@gmail.com>
2026-06-10 08:36:27 -0300

    test: Adding method isOpen and isClose for the page elements and locate all the buttons

--- src/test/java/com/hotel/booking/ifsp/ui/pages/BookingDetailModelPage.java
+++ src/test/java/com/hotel/booking/ifsp/ui/pages/BookingDetailModelPage.java
@@ -1,6 +1,9 @@
 package com.hotel.booking.ifsp.ui.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -10,10 +13,26 @@ public class BookingDetailModelPage {
     private final WebDriver driver;
     private final WebDriverWait wait;
 
+    private static final By guestName = By.xpath("//div[contains(@class,'max-w-lg')]//h2");
+    private static final By closingButton = By.xpath("//div[contains(@class,'max-w-lg')]//button[contains(@class,'rounded-full')]");
+    private static final By cancelButton = By.xpath("//div[contains(@class,'max-w-lg')]//button[contains(.,'Cancelar')]");
+    private static final By checkinButton = By.xpath("//div[contains(@class,'max-w-lg')]//button[contains(.,'Check-in')]");
+    private static final By checkoutButton = By.xpath("//div[contains(@class,'max-w-lg')]//button[contains(.,'Finalizar estadia')]");
+
     public BookingDetailModelPage(WebDriver driver){
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
+    public boolean isOpen(){
+        try{
+            wait.until(ExpectedConditions.visibilityOfElementLocated(guestName));
+            return true;
+        } catch (Exception e){
+            return false;
+        }
+    }
+
+    public boolean isClose(){}
 
 }
