6d186ad9f677d5ff6339f8574650c55cc16d8786
Filipe Braga <filipebraga0505@gmail.com>
2026-06-09 22:15:26 -0300

    test: Criando teste de caso bem sucedido no controller

---
+++ src/test/java/com/hotel/booking/ifsp/controller/TransactionControllerTest.java
@@ -0,0 +1,29 @@
+package com.hotel.booking.ifsp.controller;
+
+import io.restassured.http.ContentType;
+import org.apiguardian.api.API;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.notNullValue;
+
+
+public class TransactionControllerTest extends ApiIntegrationTestBase {
+
+    @Test
+    @Tag("ApiTest")
+    @Tag("IntegrationTest")
+    @DisplayName("GET /hello with valid token returns 200 and user id")
+    void HelloWithValidTokenReturns200AndUserId() {
+        given()
+                .header("Authorization", "Bearer " + getAdminToken())
+                .when().get("/api/v1/hello")
+                .then().statusCode(200)
+                .body(notNullValue())
+                .body(containsString("Hello: "));
+    }
+
+}
