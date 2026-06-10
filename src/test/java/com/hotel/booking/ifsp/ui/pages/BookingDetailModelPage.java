650d373acc0fc9f89f3fc815f47716caa3c90adf
Filipe Braga <filipebraga0505@gmail.com>
2026-06-10 08:26:56 -0300

    feat: Criando pagina de bookingDetail

---
+++ src/test/java/com/hotel/booking/ifsp/ui/pages/BookingDetailModelPage.java
@@ -0,0 +1,19 @@
+package com.hotel.booking.ifsp.ui.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BookingDetailModelPage {
+
+    private final WebDriver driver;
+    private final WebDriverWait wait;
+
+    public BookingDetailModelPage(WebDriver driver){
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+
+}
